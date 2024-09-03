354c91aab75a474b35e6dbfbe6c374da38feb1fa
ProfessorLagg <futurecake2999@gmail.com>
2024-09-03 22:19:43 +0200

    tried my hand at spring

---
+++ rpgtodoapi/src/main/java/net/laggserver/rpgtodoapi/RpgtodoapiApplication.java
@@ -0,0 +1,13 @@
+package net.laggserver.rpgtodoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgtodoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpgtodoapiApplication.class, args);
+	}
+
+}
